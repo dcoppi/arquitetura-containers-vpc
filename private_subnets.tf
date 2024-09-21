fe4e71c8fe77cd64263b36f070bfd8ab115d312b
Daniel Coppim <dcoppi@gmail.com>
2024-09-21 15:46:30 -0300

    day1 ajustes

--- private_subnets.tf
+++ private_subnets.tf
@@ -11,19 +11,19 @@ resource "aws_subnet" "public_subnet_1a" {
 resource "aws_subnet" "public_subnet_1b" {
     vpc_id = aws_vpc.main.id
     cidr_block = "10.0.49.0/24"
-    availability_zone = format("%sa",var.region)
+    availability_zone = format("%sb",var.region)
 
     tags = {
-        Name = format("%s-public-subnet-1a", var.project_name) 
+        Name = format("%s-public-subnet-1b", var.project_name) 
     }
 }
 
 resource "aws_subnet" "public_subnet_1c" {
     vpc_id = aws_vpc.main.id
     cidr_block = "10.0.50.0/24"
-    availability_zone = format("%sa",var.region)
+    availability_zone = format("%sc",var.region)
 
     tags = {
-        Name = format("%s-public-subnet-1a", var.project_name) 
+        Name = format("%s-public-subnet-1c", var.project_name) 
     }
 }
